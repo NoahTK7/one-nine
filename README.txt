a3dc194659ade5e9a0d464989b53784ce6a83ae9
PeteyDog7 <bontkurrack@gmail.com>
2016-03-23 14:16:27 -0400

    licensing

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 A Minecraft mod. Mincraft version 1.9, Forge version 1.9-12.16.0.1776-1.9.
+
+Please see the info directory for additional credits, licensing, and other related information.
--- src/main/java/com/noahkurrack/onenine/OneNine.java
+++ src/main/java/com/noahkurrack/onenine/OneNine.java
@@ -4,7 +4,6 @@ import com.noahkurrack.onenine.proxy.IProxy;
 import com.noahkurrack.onenine.reference.ModRef;
 import com.noahkurrack.onenine.util.LogHelper;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -13,7 +12,6 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.apache.logging.log4j.Level;
 
 @Mod(modid = ModRef.MOD_MODID, name = ModRef.MOD_NAME, version = ModRef.MOD_VERSION)
 public class OneNine {
